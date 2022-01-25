5ea2da6d33028244d5ee20d420fa11e574fdbc94
marcello <mcfanda@gmail.com>
2022-01-25 18:59:38 +0100

    updates

--- R/functions.R
+++ R/functions.R
@@ -348,3 +348,15 @@ fixRd<-function(rd) {
   Rdpack::Rdo_replace_section(rd,rdvalue)
 }
 
+
+foldable_title<-function(title,comment=NULL) {
+ a<-'<button type="button" class="collapsible">'
+ a<- paste(a,fa("expand-alt", fill = "steelblue"))
+ a<- paste(a,'<span class="colltitle">',title,"</span>")
+ a<- paste(a,'<span class="collinfo">click to read</span>')
+ if (!is.null(comment))
+    a<-paste(a,' <span class="collinfo">',comment,'</span>')
+  a<-paste(a,'</button>')
+  a<-paste(a,'<div class="collapsiblecontent">')
+  a
+}
