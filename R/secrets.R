ef95758ae8def2e4aa89c96d883b7e103f6ad0ff
marcello <mcfanda@gmail.com>
2022-01-26 18:29:59 +0100

    bad secrets

--- R/secrets.R
+++ R/secrets.R
@@ -1,3 +1,4 @@
-API_TOKEN<-"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"
 ### this was generated from https://github.com/settings/tokens on 25/1/22
 API_TOKEN<-"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"
+
+# bad idea
