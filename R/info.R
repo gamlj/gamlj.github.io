da3a1c7c2bfddf13e38196a31783b2f97b5baef1
marcello <mcfanda@gmail.com>
2022-05-25 14:23:28 +0200

    updates

---
+++ R/info.R
@@ -0,0 +1,27 @@
+# folders should be absolute or relative to the docssource folder
+# do not use the trailing slash
+MODULE_NAME="GAMLj"
+MODULE_FOLDER="/home/marcello/Skinner/Forge/jamovi/gamlj/gamlj"
+SOURCE_FOLDER="pubs"
+TARGET_FOLDER="../gamlj.github.io"
+PROJECT_FOLDER="/home/marcello/Skinner/Forge/jamovi/gamlj/gamlj.github.io"
+STORE_FOLDER="docs"
+SOURCE_FOLDER="docssource"
+
+#DATALINK<-"https://raw.githubusercontent.com/mcfanda/gamlj_docs/master/data"
+DATALINK="https://github.com/gamlj/gamlj.github.io/blob/master/data/"
+# These handle the release notes from commits
+MODULE_REPO="gamlj"
+MODULE_REPO_OWNER="gamlj"
+MODULE_LINK="https://github.com/gamlj/gamlj/"
+MODULE_EMAIL="mcfanda@gmail.com
+
+"
+
+FIRST_VERSION="Version.1.0.5"
+BANNED_COMMITS=list("initialize","fix commits","remove some marks","typo in random table #73","1.5.0")
+BANNED_COMMITS_GREP=list("^#","^!","^Merge branch","spelling","2.9.","2.9.","2.9.")
+
+# These handle the R vignettes
+
+VIGNETTES_FOLDER="/home/marcello/Skinner/Forge/jamovi/gamlj/gamlj/vignettes/"
---
+++ R/newversion.R
@@ -0,0 +1,8 @@
+#remotes::install_github("mcfanda/mcdocs")
+library(mcdocs)
+mcdocs_init()
+## this renders the website
+render_mcdocs()
+## this update your github site
+update_git()
+
