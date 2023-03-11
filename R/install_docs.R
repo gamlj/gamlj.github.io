3aac464e1d8d0f414b025eaf9cada8ebb2544602
mcfanda <mcfanda@gmail.com>
2023-03-11 03:33:52 +0100

    Today wasn't so bad

---
+++ R/install_docs.R
@@ -0,0 +1,18 @@
+#remotes::install_github("mcfanda/mcdocs")
+library(mcdocs)
+
+
+bookdown::render_book("gitdocs/",output_dir = "../docs/",clean = T)
+
+pkgdown::build_site(preview = FALSE)
+mcdocs_init()
+
+str(knitr::knit_engines$get()) 
+## this renders the website
+#render_mcdocs()
+
+## this update your github site
+#update_git()
+
+
+# old tag G-2YGWBQCQK3
