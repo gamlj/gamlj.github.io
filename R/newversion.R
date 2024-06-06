cec1a5733b17a10aff1f1eb35a216b4679e22c31
mcfanda <mcfanda@gmail.com>
2024-06-06 12:44:46 +0200

    updates

--- R/newversion.R
+++ R/newversion.R
@@ -3,10 +3,10 @@ library(mcdocs)
 mcdocs_init()
 
 ## this renders the website
-render_mcdocs()
+output_format = "rmdformats::readthedown"
+render_mcdocs(output_format)
+
 bookdown::render_book("gitdocs/",output_dir = "../docs/book",clean = T)
-
-
 pkgdown::build_site(pkg = "~/Skinner/Forge/jamovi/gamlj/gamlj")
 cmd<-"mv ~/Skinner/Forge/jamovi/gamlj/gamlj/docs ~/Skinner/Forge/jamovi/gamlj/gamlj.github.io/docs/rpackage"
 system(cmd)
