0ad5fe96aef5701e416d1cbd170f3966ecc45365
mcfanda <mcfanda@gmail.com>
2024-08-21 15:17:29 +0200

    updates

--- R/install.R
+++ R/install.R
@@ -6,10 +6,10 @@ mcdocs_init()
 output_format = "rmdformats::readthedown"
 render_mcdocs(output_format)
 
-#bookdown::render_book("gitdocs/",output_dir = "../docs/book",clean = T)
-#pkgdown::build_site(pkg = "~/Skinner/Forge/jamovi/gamlj/gamlj")
-#cmd<-"mv ~/Skinner/Forge/jamovi/gamlj/gamlj/docs ~/Skinner/Forge/jamovi/gamlj/gamlj.github.io/docs/rpackage"
-#system(cmd)
+bookdown::render_book("gitdocs/",output_dir = "../docs/book",clean = T)
+pkgdown::build_site(pkg = "~/Skinner/Forge/jamovi/gamlj/gamlj")
+cmd<-"mv ~/Skinner/Forge/jamovi/gamlj/gamlj/docs ~/Skinner/Forge/jamovi/gamlj/gamlj.github.io/docs/rpackage"
+system(cmd)
 
 ## this update your github osite
 update_git()
